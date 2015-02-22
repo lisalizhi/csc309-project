517de0fa6fcbeddec5a0daa2afcb81b6e5aa7445
Leora R <lrogovein@hotmail.ca>
2015-02-22 17:02:24 -0500

    Updates to header&footer, pls read description
    I've moved the header and footer into separate html files. Please make
    all new pages as .php, and put the statements <?php
    require('header.html');?> at the top of the body, and <?php
    require('footer.html');?> at the bottom before </body> tag.  The index
    page currently has its own unique header, because it has a slightly
    different styles than all the other pages (ie. it has a background
    image). We can easily change this if we need to. If you are confused, go
    look at my listing.php file.

---
+++ SynergySpace/index.php
@@ -0,0 +1,73 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>SynergySpace</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="index page"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+	<section class="hero">
+		<header>
+			<div class="wrapper">
+				<a href="index.php"><img src="img/logo.png" class="logo" alt="" titl=""/></a>
+				<a href="#" class="hamburger"></a>
+				<nav>
+					<ul>
+						<li><a href="#">Profile</a></li>
+						<li><a href="#">Rent</a></li>
+						<li><a href="listings.php">Listings</a></li>
+						<li><a href="#about">About</a></li>
+					</ul>
+					<a href="#" class="login_btn">Login</a>
+                    <a href ="#" class = "login_btn"> Register </a>
+				</nav>
+			</div>
+		</header><!--  end header section  -->
+
+	<section class="search">
+		<div class="wrapper">
+			<form action="#" method="post">
+				<input type="text" id="search" name="search" placeholder="What are you looking for?"  autocomplete="off"/>
+				<input type="submit" id="submit_search" name="submit_search"/>
+			</form>
+			<a href="#" class="advanced_search_icon" id="advanced_search_btn"></a>
+		</div>
+
+		<div class="advanced_search">
+			<div class="wrapper">
+				<span class="arrow"></span>
+				<form action="#" method="post">
+					<div class="search_fields">
+						<input type="text" class="float" id="min_price" name="min_price" placeholder="Min. Price"  autocomplete="off">
+
+						<hr class="field_sep float"/>
+
+						<input type="text" class="float" id="max_price" name="max_price" placeholder="Max. price"  autocomplete="off">
+					</div>
+					<input type="text" id="keywords" name="keywords" placeholder="Keywords"  autocomplete="off">
+					<input type="submit" id="submit_search" name="submit_search"/>
+				</form>
+			</div>
+		</div><!--  end advanced search section  -->
+
+	</section><!--  end search section  -->
+
+			<section class="caption">
+				<h2 class="caption">SynergySpace</h2>
+				<h3 class="properties">Teaming to Succeed</h3>
+			</section>
+	</section><!--  end beginning of header  -->
+
+<?php require('footer.html');?>
+	
+</body>
+</html>
--- SynergySpace/listings.php
+++ SynergySpace/listings.php
@@ -1,5 +1,5 @@
-﻿<!DOCTYPE html>
-<!-- original template by pixelhint.com -->
+<!DOCTYPE html>
+<!-- original template by pixelhint.com, modified substantially by the group -->
 <html lang="en">
 <head>
 	<title>SynergySpace</title>
@@ -14,54 +14,10 @@
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
 </head>
+
 <body>
 
-	<section class="topper">
-		<header>
-			<div class="wrapper">
-				<a href="#"><img src="#" class="logo" alt="" titl=""/></a>
-				<a href="#" class="hamburger"></a>
-				<nav>
-					<ul>
-						<li><a href="#">Profile</a></li>
-						<li><a href="#">Rent</a></li>
-						<li><a href="#">Listings</a></li>
-						<li><a href="#about">About</a></li>
-					</ul>
-					<a href="#" class="login_btn">Login</a>
-                    <a href ="#" class = "login_btn"> Register </a>
-				</nav>
-			</div>
-		</header><!--  end header section  -->
-
-
-	<section class="search">
-		<div class="wrapper">
-			<form action="#" method="post">
-				<input type="text" id="search" name="search" placeholder="What are you looking for?"  autocomplete="off"/>
-				<input type="submit" id="submit_search" name="submit_search"/>
-			</form>
-			<a href="#" class="advanced_search_icon" id="advanced_search_btn"></a>
-		</div>
-
-		<div class="advanced_search">
-			<div class="wrapper">
-				<span class="arrow"></span>
-				<form action="#" method="post">
-					<div class="search_fields">
-						<input type="text" class="float" id="min_price" name="min_price" placeholder="Min. Price"  autocomplete="off">
-
-						<hr class="field_sep float"/>
-
-						<input type="text" class="float" id="max_price" name="max_price" placeholder="Max. price"  autocomplete="off">
-					</div>
-					<input type="text" id="keywords" name="keywords" placeholder="Keywords"  autocomplete="off">
-					<input type="submit" id="submit_search" name="submit_search"/>
-				</form>
-			</div>
-		</div><!--  end advanced search section  -->
-	</section><!--  end search section  -->
-
+	<?php require('header.html');?>
 
 	<section class="listings">
 		<div class="wrapper">
@@ -181,55 +137,6 @@
 		</div>
 	</section>	<!--  end listing section  -->
 
-	<footer>
-		<div class="wrapper footer">
-			<ul>
-				<li class="links">
-					<ul>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Support</a></li>
-						<li><a href="#">Terms</a></li>
-						<li><a href="#">Policy</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-				</li>
-
-				<li class="links">
-					<ul>
-						<li><a href="#">Appartements</a></li>
-						<li><a href="#">Houses</a></li>
-						<li><a href="#">Villas</a></li>
-						<li><a href="#">Mansions</a></li>
-						<li><a href="#">...</a></li>
-					</ul>
-				</li>
-
-				<li class="links">
-					<ul>
-						<li><a href="#">New York</a></li>
-						<li><a href="#">Los Anglos</a></li>
-						<li><a href="#">Miami</a></li>
-						<li><a href="#">Washington</a></li>
-						<li><a href="#">...</a></li>
-					</ul>
-				</li>
-
-				<li class="about">
-					<p>SynergySpace</p>
-					<ul>
-						<li><a href="http://facebook.com/pixelhint" class="facebook" target="_blank"></a></li>
-						<li><a href="http://twitter.com/pixelhint" class="twitter" target="_blank"></a></li>
-						<li><a href="http://plus.google.com/+Pixelhint" class="google" target="_blank"></a></li>
-						<li><a href="#" class="skype"></a></li>
-					</ul>
-				</li>
-			</ul>
-		</div>
-
-		<div class="copyrights wrapper">
-			Copyright © 2015 <a href="http://pixelhint.com" target="_blank" class="ph_link" title="Download more free Templates">Pixelhint.com</a>. All Rights Reserved.
-		</div>
-	</footer><!--  end footer  -->
-	
+	<?php require('footer.html');?>
 </body>
 </html>
