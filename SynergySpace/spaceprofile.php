61198178b34774d0a519241788cfb799f1482fd2
mertec <c.merte.marktwo@gmail.com>
2015-03-08 16:53:17 -0400

    Create Space Profile
    Almost done I think...

---
+++ SynergySpace/spaceprofile.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- original template by pixelhint.com, modified substantially by the ArgoBots group -->
+<html lang="en">
+<head>
+	<title>SynergySpace</title>
+	<meta charset="utf-8">
+	<meta name="author" content="ArgoBots">
+	<meta name="description" content="index page"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="css/formpages.css">
+
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	
+</head>
+<body>
+
+	<?php require('header.html');?>
+
+	
+		<section class="logform">
+		<div class="wrapper">
+		<br>
+		<h1>Create a Space Listing</h1>
+
+				<form action="#" method="get">
+					<div class="search_fields">
+
+						<hr class="form_horiz"/>
+						SpaceID:<br>
+						<input type="text" class="float" id="logform" name="SID"  autocomplete="off">
+						<br>
+						<hr class="form_horiz"/>
+
+						Owner Name:<br>
+						<input type="text" class="float" id="logform" name="owner"  autocomplete="off">
+						<br>
+						<hr class="form_horiz"/>
+				
+						Space Location:<br>
+						<input type="text" class="float" id="logform" name="location"   autocomplete="off">
+						<br>
+						<hr class="form_horiz"/>
+
+						Price:<br>
+						<input type="text" class="float" id="logform" name="monaaaay"  autocomplete="off">
+						<br>
+						<hr class="form_horiz"/>
+
+						Space Description:<br>
+						<textarea name="description" rows="15" cols="45"></textarea>
+					</div>
+					<hr class="form_horiz"/>					
+					<input type="submit" id="submit_search" class="form_button" name="submit_search"/>
+				</form>	
+		</div>
+	</section>
+
+
+<?php require('footer.html');?>
+</body>	
+
+</html>	
