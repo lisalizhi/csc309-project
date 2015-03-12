dd80e30d289f55f73284553297cc58b9b7990c69
mertec <c.merte.marktwo@gmail.com>
2015-03-12 18:23:06 -0400

    Edit

--- SynergySpace/controller/makespace.php
+++ SynergySpace/controller/makespace.php
@@ -1,37 +1,40 @@
-<?php			
+<?php session_start();?>			
 		require('connect.php');
 			
-		if (isset($_POST['submit_search'])){  
-			$location = $_POST['location'];
-			$price = $_POST['price'];
-			$description = $_POST['description'];
+		if (isset($_POST['submit_search'])){ 
+			if (isset($_SESSION['username'])) {
+				$_SESSION['username'];
+				$location = $_POST['location'];
+				$price = $_POST['price'];
+				$description = $_POST['description'];
 			
-			$location = htmlspecialchars($location);// changes characters used in html to their equivalents,ex. < to &gt;
-			$price = htmlspecialchars($price);
-			$description = htmlspecialchars($description);
+				$location = htmlspecialchars($location);<!-- changes characters used in html to their equivalents,ex. < to &gt; -->
+				$price = htmlspecialchars($price);
+				$description = htmlspecialchars($description);
 			
-			$location = mysql_real_escape_string($location);// makes sure nobody uses SQL injection
-			$price = mysql_real_escape_string($price);
-			$description = mysql_real_escape_string($description);
+				$location = mysql_real_escape_string($location);<!-- makes sure nobody uses SQL injection -->
+				$price = mysql_real_escape_string($price);
+				$description = mysql_real_escape_string($description);
 			
-			if($location and $price and $description){
+				if($location and $price and $description){
 							
-				if(mysql_num_rows($raw_results) == 0){ 
-					//insert information into the space table 
-					$sql = "INSERT INTO spaces (location, price, description, photo) VALUES ('$location', '$price', '$description', ' ')";	
-					$retval = mysql_query($sql);
-					if(!$retval ){//error handling
-						die('Could not enter data: ' . mysql_error());
-					}else{
-						header('Location: ../index.php');//redirects user to the indicated page when when teh info is successfully added to database
+					if(mysql_num_rows($raw_results) == 0){ 
+						<!-- insert information into the space table -->
+						$sql = "INSERT INTO spaces (location, price, description, photo) VALUES ('$location', '$price', '$description', ' ')";	
+						$retval = mysql_query($sql);
+						if(!$retval ){//error handling
+							die('Could not enter data: ' . mysql_error());
+						}else{
+							header('Location: ../index.php');<!-- redirects user to the indicated page when when the info is successfully added to database -->
+						}
 					}
+					else{ <!-- if there is no matching rows do following -->
+						echo "Username or email already exist";
+					}
+				}else{
+					echo "Missing fields";
 				}
-				else{ // if there is no matching rows do following
-					echo "Username or email already exist";
-				}
-			}else{
-				echo "Missing fields";
-			}
 			
-		}			
+			}
+		}
 	?>
