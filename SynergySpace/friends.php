843a56af3247017bf4d1fe8e17d96a936dc8b1ed
juliekd <julikatd@gmail.com>
2015-04-03 20:23:41 -0400

    View Friends in profile and own page

--- SynergySpace/friends.php
+++ SynergySpace/friends.php
@@ -35,19 +35,6 @@
 				$raw_results = mysql_query("SELECT * FROM users
 						WHERE username='".$username."'");
 				$results = mysql_fetch_array($raw_results);
-				
-				//get spaces user owns
-				$raw_ownspaces = mysql_query("SELECT * FROM space
-						WHERE ownerusername='".$username."'");
-						
-				//get info on where user works
-				$raw_w = mysql_query("SELECT * FROM members
-						WHERE username='".$username."'");
-				$w = mysql_fetch_array($raw_w);	
-				$w_sid = $w['sid'];
-				$w_sid = htmlspecialchars($w_sid);
-				$raw_works = mysql_query("SELECT * FROM space WHERE sid='".$w_sid."'");
-				$works = mysql_fetch_array($raw_works);	
 			}
 		?> 
 		
@@ -58,70 +45,26 @@
 				<div class="backlinks">
 					<div class="insidelinks">
 						<img src="uploads/<?=$results['photo']?>" width="200" alt="Profile Picture!" />
-						<h5> Age: <?php echo "".$results['age']."";?></h5>
-						<h5> Location: <?php echo "".$results['location']."";?></h5>
-						<h5> Works at: <a href="spaceprofile.php?sid=<?=$works['sid']?>"><?=$works['name']?></a></h5>
-						<h5> Score: <?php echo "".$results['avescore']."";?> </h5>
+						<h5> Name: <?=$results['first']?> <?=$results['last']?></h5>
+						<h5> Occupation: <?=$results['occupation']?></h5>
+						<h5> Location: <?=$results['location']?></h5>
 						
-						<?php
-						if (isset($_SESSION['username']) and $username == $_SESSION['username']){ ?>
-							
-							<form action="editprofile.php" method="post">				
-								<input type="submit" id="edit_profile" class="form_button" name="edit_profile" value="Edit"/>
-							</form>
-							<br><br>
-						<?php } ?>
 						
 					</div>
 				</div>
 				<div class="mainprof">
 					<h2> 
-					<?php 
-						//if user is logged in, print their username
-						echo "".$results['first']." ".$results['last']."";				
-					?> 
+					<?=$results['first']?>'s Friends
 					</h2>
-					<h3>Occupation: <?php echo "".$results['occupation']."";?></h3>
-					<p>Email: <?php echo "".$results['email']."";?></p>
-					<hr class="profbreak" />				
-					<p><?php echo "".$results['description']."";?></p>
-				</div>
-			
-				<br>
-				<div class="mainprof">
-					<h3> Spaces </h3>
 					<hr class="profbreak" />
-					<?php
-					if(mysql_num_rows($raw_ownspaces) > 0){ // if one or more rows are returned do following
-						// $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it prints the formatted data in the loop
-						while($ownspaces = mysql_fetch_array($raw_ownspaces)){ 
-							//output formatted results
-							?>
-							<a href="spaceprofile.php?sid=<?=$ownspaces['sid']?>"><h4><?=$ownspaces['name']?></h4></a>
-							<br>
-							<?php 
-						}			 
-					}
-					else{ // if there is no matching rows do following
-						echo "No results";
-					}	
-					?>
-				</div>
-				
-				<br>
-				<div class="mainprof">
-					<h3> Friends </h3>
-					<hr class="profbreak" />
-					
 					<?php
 					if (isset($_GET['u'])) {
 						$user = $_GET['u'];
 						$user = htmlspecialchars($user);
-						$i = 0;
 						$friendresults = mysql_query("SELECT * FROM friendswith
 							WHERE username1='$user' OR username2 = '$user'");
 						if(mysql_num_rows($friendresults) > 0){
-							while($results = mysql_fetch_array($friendresults) and $i < 4){
+							while($results = mysql_fetch_array($friendresults)){
 								if ($results['username1'] == $user){
 									$friend = $results['username2'];
 								}else{
@@ -139,22 +82,15 @@
 								<?php
 
 								}
-								$i++;
-							}
-						?>
-						<p><a href="friends.php?u=<?=$username?>">View All</a></p>
-						<?php
+							}							
 						}else{ ?>
-						<h5>No results</h5>
+						<h4>User has no friends! :(</h4>
 						<?php
 							
 						}
 					}?>
-					
-					
-					
+
 				</div>
-			</div>
 		</div>
 	</section>	
 
